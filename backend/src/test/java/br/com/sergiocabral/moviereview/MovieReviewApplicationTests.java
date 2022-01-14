94b6a0b9b5035301a7839298193f303058e501f1
Sergio_Cabral <scastrocabral@gmail.com>
2022-01-14 15:26:39 -0300

    Create project

---
+++ backend/src/test/java/br/com/sergiocabral/moviereview/MovieReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sergiocabral.moviereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
